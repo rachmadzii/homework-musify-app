ff1f1bb907f849c909b035cef301ac0a9977114d
Amaa <rachmadzima30@gmail.com>
2022-03-19 18:43:19 +0700

    Create new react app with API key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const API_KEY = process.env.REACT_APP_API_KEY;
   return (
     <div className="App">
       <header className="App-header">
