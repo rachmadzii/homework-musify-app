121eb0ac620141d46b6285b6add8d401cde6c9c2
Amaa <rachmadzima30@gmail.com>
2022-03-25 10:48:33 +0700

    homework: component props track playlist

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import data from "./data.js";
+import Track from "./components/Track";
 
 function App() {
   // const API_KEY = process.env.REACT_APP_API_KEY;
   return (
     <div className="container">
       <h1>My Playlist</h1>
-      <div className="cardPlaylist">
-        <img src={data.album.images[0].url} alt="ImagePlaylist" />
-        <h3>{data.album.name}</h3>
-        <p>{data.album.artists[0].name}</p>
-        <button className="btnSelect">Select</button>
-      </div>
+      <Track
+        url={data.album.images[0].url}
+        title={data.album.name}
+        artist={data.album.artists[0].name}
+      />
     </div>
   );
 }
