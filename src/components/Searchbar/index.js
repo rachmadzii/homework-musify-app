5b359a4304fc9ac8f4709327847894b1eb1ad99b
Amaa <rachmadzima30@gmail.com>
2022-04-04 12:41:59 +0700

    fix: use function searchTrack

--- src/components/Searchbar/index.js
+++ src/components/Searchbar/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import config from "../../utils/config";
+import { searchTrack } from "../../utils/fetchApi";
 
 export default function SearchBar({ accessToken, onSuccess, onClearSearch }) {
   const [text, setText] = useState("");
@@ -11,20 +11,10 @@ export default function SearchBar({ accessToken, onSuccess, onClearSearch }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const requestOptions = {
-      headers: {
-        Authorization: "Bearer " + accessToken,
-        "Content-Type": "application/json",
-      },
-    };
-
     try {
-      const response = await fetch(
-        `${config.SPOTIFY_BASE_URL}/search?type=track&q=${text}`,
-        requestOptions
-      ).then((data) => data.json());
+      const responseSearch = await searchTrack(text, accessToken);
 
-      const tracks = response.tracks.items;
+      const tracks = responseSearch.tracks.items;
       onSuccess(tracks);
     } catch (e) {
       alert(e);
