64dc7df3e01c26df995117c3361ed5b12d32f0f9
Rachmadzii <rachmadzima4@gmail.com>
2022-04-08 00:42:44 +0700

    homework: update route Create Playlist for logged in user

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
+import CreatePlaylist from "./pages/CreatePlaylist";
+import Login from "./pages/Login";
 
 function App() {
-  return <Home />;
+  const isAuthorized = useSelector((state) => state.auth.isAuthorized);
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/create-playlist" exact>
+          {isAuthorized ? <CreatePlaylist /> : <Redirect to="/" />}
+        </Route>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
